da451b1dbd10289d7eb829c15b4bac6b4554ee52
Greta Chang <greta.chang@razerzone.com>
2019-03-10 17:03:15 -0700

    remove permissions request. looks like we don't need this?

--- app/src/main/java/com/kemikalreaktion/helios/MainActivity.kt
+++ app/src/main/java/com/kemikalreaktion/helios/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.kemikalreaktion.helios
 
-import android.Manifest.permission.SET_WALLPAPER
-import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
 
-const val SET_WALLPAPER_PERMISSION_REQUEST = 0
 class MainActivity : AppCompatActivity() {
     private lateinit var mWallpaperHelper : WallpaperHelper
-    private var hasPermissions = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,50 +17,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        if (checkSelfPermission(SET_WALLPAPER) != PackageManager.PERMISSION_GRANTED) {
-            // Request permission here
-            /*if (shouldShowRequestPermissionRationale(SET_WALLPAPER)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {*/
-            // No explanation needed, we can request the permission.
-            requestPermissions(arrayOf(SET_WALLPAPER), SET_WALLPAPER_PERMISSION_REQUEST)
-
-            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-            // app-defined int constant. The callback method gets the
-            // result of the request.
-            //}
-        } else {
-            hasPermissions = true
-        }
     }
 
     override fun onResume() {
         super.onResume()
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        when (requestCode) {
-            SET_WALLPAPER_PERMISSION_REQUEST -> {
-                // If request is cancelled, the result arrays are empty.
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                    hasPermissions = true;
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return
-            }
-        }
-    }
-
     fun onApplyClicked(view: View) {
-        if (hasPermissions) {
-            mWallpaperHelper.setWallpaper();
-        }
+        mWallpaperHelper.setWallpaper();
         Toast.makeText(this, "Wallpaper applied!", LENGTH_SHORT).show()
     }
 
